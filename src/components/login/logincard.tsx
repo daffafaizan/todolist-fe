5a884307d6595c5511aee54442442914ab9bc322
Daffa <daffamfzn@gmail.com>
2024-01-15 17:58:53 +0700

    feat: added login logic

--- src/components/login/logincard.tsx
+++ src/components/login/logincard.tsx
@@ -1,20 +1,60 @@
 "use client";
 
+import { navigate } from "@/actions/redirect";
+import { useState } from "react";
+
 function LoginCard() {
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/auth/login`;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      if (response.ok) {
+        setUsername("");
+        setPassword("");
+
+        navigate("/todolist");
+      } else {
+        console.error("Login failed");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
   return (
     <div className="dark:text-white text-stone-900 w-64 h-64 max-w-md flex flex-col items-center justify-center p-4 gap-3">
       <div className="text-2xl underline underline-offset-8 decoration-4 decoration-cyan-500 font-semibold">
         <span>Login</span>
       </div>
-      <form className="mt-4 flex flex-col items-center gap-2">
+      <form
+        onSubmit={handleSubmit}
+        className="mt-4 flex flex-col items-center gap-2"
+      >
         <input
           className="w-full rounded-md bg-[#edefe7] placeholder:text-sm px-2 py-1"
           placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.currentTarget.value)}
+          required
         />
         <input
           type="password"
           className="w-full rounded-md bg-[#edefe7] placeholder:text-sm px-2 py-1"
           placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
+          required
         />
         <button className="mt-4 w-full inline-flex justify-center rounded-md border border-transparent bg-cyan-500 px-2 py-1 text-sm font-medium text-white hover:bg-cyan-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
           Sign in
