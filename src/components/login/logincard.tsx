b2c7ab5f5e2ba1804d700c3e67a8347a6ecbf404
Daffa <daffamfzn@gmail.com>
2024-01-15 16:45:13 +0700

    style: finished styling the register and login pages

--- src/components/login/logincard.tsx
+++ src/components/login/logincard.tsx
@@ -2,16 +2,17 @@
 
 function LoginCard() {
   return (
-    <div className="dark:text-white text-stone-900 w-64 h-64 max-w-md flex flex-col items-center justify-center p-4 gap-6">
+    <div className="dark:text-white text-stone-900 w-64 h-64 max-w-md flex flex-col items-center justify-center p-4 gap-3">
       <div className="text-2xl underline underline-offset-8 decoration-4 decoration-cyan-500 font-semibold">
         <span>Login</span>
       </div>
-      <form className="flex flex-col items-center gap-2">
+      <form className="mt-4 flex flex-col items-center gap-2">
         <input
           className="w-full rounded-md bg-[#edefe7] placeholder:text-sm px-2 py-1"
           placeholder="Username"
         />
         <input
+          type="password"
           className="w-full rounded-md bg-[#edefe7] placeholder:text-sm px-2 py-1"
           placeholder="Password"
         />
@@ -19,6 +20,12 @@ function LoginCard() {
           Sign in
         </button>
       </form>
+      <div className="text-xs flex flex-row gap-1">
+        <span>Don`&apos`t have an account?</span>
+        <a className="text-cyan-500 hover:text-cyan-600" href="/register">
+          Register
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/register/registercard.tsx
+++ src/components/register/registercard.tsx
@@ -2,11 +2,11 @@
 
 function RegisterCard() {
   return (
-    <div className="dark:text-white text-stone-900 w-64 h-64 max-w-md flex flex-col items-center justify-center p-4 gap-6">
+    <div className="dark:text-white text-stone-900 w-64 h-64 max-w-md flex flex-col items-center justify-center p-4 gap-3">
       <div className="text-2xl underline underline-offset-8 decoration-4 decoration-cyan-500 font-semibold">
         <span>Register</span>
       </div>
-      <form className="flex flex-col items-center gap-2">
+      <form className="mt-4 flex flex-col items-center gap-2">
         <input
           className="w-full rounded-md bg-[#edefe7] placeholder:text-sm px-2 py-1"
           placeholder="Name"
@@ -16,6 +16,7 @@ function RegisterCard() {
           placeholder="Username"
         />
         <input
+          type="password"
           className="w-full rounded-md bg-[#edefe7] placeholder:text-sm px-2 py-1"
           placeholder="Password"
         />
@@ -23,6 +24,12 @@ function RegisterCard() {
           Sign up
         </button>
       </form>
+      <div className="text-xs flex flex-row gap-1">
+        <span>Already registered?</span>
+        <a className="text-cyan-500 hover:text-cyan-600" href="/login">
+          Login
+        </a>
+      </div>
     </div>
   );
 }
